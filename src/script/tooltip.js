afce04d781da54ab5d5e20e3a37afe426361d261
Sharky <sharkyblacktip@protonmail.com>
2025-04-07 20:48:57 +0200

    Created a tooltip thingy

---
+++ src/script/tooltip.js
@@ -0,0 +1,34 @@
+// NEEDS to be deffered
+
+const tooltip = document.createElement("div");
+tooltip.id = "tooltip";
+tooltip.innerHTML = "this is a tooltip";
+
+const offset_px = 12;
+
+// Attatch the tooltip
+document.body.appendChild(tooltip);
+
+// Listen to mouse move events
+document.addEventListener(
+  "mousemove",
+  (event) => {
+    tooltip.style.top = `${event.clientY + offset_px}px`;
+    tooltip.style.left = `${event.clientX + offset_px}px`;
+
+    const target = document.elementFromPoint(event.clientX, event.clientY);
+    if (target && target.classList.contains("show-tooltip")) {
+      tooltip.style.opacity = 1;
+
+      if (target.getAttribute("data-tooltip")) {
+        tooltip.innerHTML = target.getAttribute("data-tooltip");
+      } else {
+        tooltip.innerHTML =
+          "This element does not have the <code>data-tooltip</code> attribute";
+      }
+    } else {
+      tooltip.style.opacity = 0;
+    }
+  },
+  { passive: true }
+);
