f24498b0c4a522bc940a00d5e2ce94dc54257d18
Sharky <sharkyblacktip@protonmail.com>
2025-05-24 20:06:59 +0200

    Small refactor

--- src/script/tooltip.js
+++ src/script/tooltip.js
@@ -14,19 +14,25 @@ document.body.appendChild(tooltip);
 document.addEventListener(
   "mousemove",
   (event) => {
+    // Always update position
     tooltip.style.top = `${event.clientY + offset_px}px`;
     tooltip.style.left = `${event.clientX + offset_px}px`;
 
     const target = document.elementFromPoint(event.clientX, event.clientY);
-    if (target && target.getAttribute("data-tooltip")) {
-      tooltip.style.opacity = 1;
+    let show_tooltip = false;
 
-      if (target.getAttribute("data-tooltip")) {
-        tooltip.innerHTML = target.getAttribute("data-tooltip");
-      } else {
-        tooltip.innerHTML =
-          "This element does not have the <code>data-tooltip</code> attribute";
-      }
+    if (target && target.getAttribute("data-tooltip")) {
+      show_tooltip = true;
+    } else {
+      show_tooltip = false;
+    }
+
+    if (show_tooltip) {
+      // No need for checking if attribute exists because
+      // show_tooltip is only true when it does
+      tooltip.innerHTML = target.getAttribute("data-tooltip");
+
+      tooltip.style.opacity = 1;
     } else {
       tooltip.style.opacity = 0;
     }
