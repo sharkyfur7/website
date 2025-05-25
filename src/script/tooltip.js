4ab9a00106df5cad1f5268d7617ce5c39bd0c86f
Sharky <sharkyblacktip@protonmail.com>
2025-05-25 19:47:57 +0200

    Fixed bug related to scrolling

--- src/script/tooltip.js
+++ src/script/tooltip.js
@@ -7,35 +7,40 @@ tooltip.classList.add("big-screen-only");
 
 const offset_px = 12;
 
+function update(event) {
+  // Always update position
+  tooltip.style.top = `${event.clientY + window.scrollY + offset_px}px`;
+  tooltip.style.left = `${event.clientX + offset_px}px`;
+
+  const target = document.elementFromPoint(event.clientX, event.clientY);
+  let show_tooltip = false;
+
+  if (target && target.getAttribute("data-tooltip")) {
+    show_tooltip = true;
+  } else {
+    show_tooltip = false;
+  }
+
+  if (show_tooltip) {
+    // No need for checking if attribute exists because
+    // show_tooltip is only true when it does
+    tooltip.innerHTML = target.getAttribute("data-tooltip");
+
+    tooltip.style.opacity = 1;
+  } else {
+    tooltip.style.opacity = 0;
+  }
+}
+
 // Attatch the tooltip
 document.body.appendChild(tooltip);
 
 // Listen to mouse move events
+document.addEventListener("mousemove", update, { passive: true });
 document.addEventListener(
-  "mousemove",
-  (event) => {
-    // Always update position
-    tooltip.style.top = `${event.clientY + offset_px}px`;
-    tooltip.style.left = `${event.clientX + offset_px}px`;
-
-    const target = document.elementFromPoint(event.clientX, event.clientY);
-    let show_tooltip = false;
-
-    if (target && target.getAttribute("data-tooltip")) {
-      show_tooltip = true;
-    } else {
-      show_tooltip = false;
-    }
-
-    if (show_tooltip) {
-      // No need for checking if attribute exists because
-      // show_tooltip is only true when it does
-      tooltip.innerHTML = target.getAttribute("data-tooltip");
-
-      tooltip.style.opacity = 1;
-    } else {
-      tooltip.style.opacity = 0;
-    }
+  "scroll",
+  () => {
+    tooltip.style.opacity = 0;
   },
   { passive: true }
 );
