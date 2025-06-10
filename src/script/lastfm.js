4090c7cca7bb9e97359f381548290ad1127eedf9
Sharky <sharkyblacktip@protonmail.com>
2025-06-10 21:18:09 +0200

    Updated lastfm wide widget

--- src/script/lastfm.js
+++ src/script/lastfm.js
@@ -1,5 +1,12 @@
-const api_url =
+const recent_tracks_api_url =
   "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" +
+  "sharkyblacktip" +
+  "&api_key=" +
+  "8093eae1b06820d1d900400320d5f8f9" +
+  "&format=json";
+
+const info_api_url =
+  "https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=" +
   "sharkyblacktip" +
   "&api_key=" +
   "8093eae1b06820d1d900400320d5f8f9" +
@@ -17,74 +24,80 @@ function formatAgo(seconds) {
   }
 }
 
-const resp = fetch(api_url)
-  .then((response) => {
-    if (!response.ok) {
-      const response = {
-        ok: false,
-      };
+async function getData() {
+  const tracks = await (await fetch(recent_tracks_api_url)).json();
+  const info = await (await fetch(info_api_url)).json();
 
-      return json(response);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    const last_track = data["recenttracks"]["track"][0];
+  const data = {
+    last_track: tracks["recenttracks"]["track"][0],
+    info: info,
+  };
 
-    const artist_name = last_track["artist"]["#text"];
-    const track_name = last_track["name"];
-    const coverurl = last_track["image"][2]["#text"]; // 0 = small, 1 = medium, 2 = large, 3 = extralarge
-    let nowplaying;
-    let date; // seconds since unix epoch, valid only when !nowplaying
+  try {
+    data.nowplaying = data.last_track["@attr"]["nowplaying"];
+  } catch (e) {}
 
-    try {
-      nowplaying = last_track["@attr"]["nowplaying"];
-    } catch (e) {}
+  try {
+    data.date = data.last_track["date"]["uts"];
+  } catch (e) {}
 
-    try {
-      date = last_track["date"]["uts"];
-    } catch (e) {}
+  if (data.nowplaying) {
+    data.date = "right now";
+  } else {
+    console.log(data.date);
+    data.date = formatAgo(Date.now() / 1000 - data.date);
+  }
 
-    if (nowplaying) {
-      date = "right now";
-    } else {
-      date = formatAgo(Date.now() / 1000 - date);
-    }
+  return data;
+}
 
-    if (date == "right now") {
-      document
-        .getElementById("cover")
-        .setAttribute("data-tooltip", "I'm listening to this song right now");
-    } else {
-      document
-        .getElementById("cover")
-        .setAttribute("data-tooltip", `I listened to this song ${date}`);
-    }
+async function main() {
+  const data = await getData();
 
-    document.getElementById("trackname").innerText = track_name;
-    document.getElementById("artist").innerText = artist_name;
-    document.getElementById("trackname").href = last_track["url"];
-    document.getElementById("cover").src = coverurl;
-    document.getElementById("bg-cover").src = coverurl;
-    document.getElementById("cover-link").href = last_track["url"];
-    document.getElementById("fmtime").innerHTML = "(" + date + ")";
+  // Now we do stuff :3
 
-    // RAVEPOP has an 18+ album cover on last.fm... plz fix :(
-    if (
-      last_track["album"]["#text"] == "RAVEPOP" &&
-      artist_name == "r u s s e l b u c k"
-    ) {
-      document.getElementById("cover").style.filter = "blur(8px)";
-    }
+  if (data.date == "right now") {
+    document
+      .getElementById("cover")
+      .setAttribute("data-tooltip", "I'm listening to this song right now");
+  } else {
+    document
+      .getElementById("cover")
+      .setAttribute("data-tooltip", `I listened to this song ${data.date}`);
+  }
 
-    if (artist_name == "Nirvana") {
-      const kurt = document.createElement("img");
-      kurt.src = "/assets/kurt.png";
-      kurt.loading = "lazy";
-      kurt.id = "kurt";
-      document.getElementById("lastfm").appendChild(kurt);
-    }
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+  const artist_name = data.last_track["artist"]["#text"];
+  const track_name = data.last_track["name"];
+  const coverurl = data.last_track["image"][2]["#text"]; // 0 = small, 1 = medium, 2 = large, 3 = extralarge
+
+  document.getElementById("trackname").innerText = track_name;
+  document.getElementById("artist").innerText = artist_name;
+  document.getElementById("albumname").innerText =
+    data.last_track["album"]["#text"];
+  document.getElementById("trackname").href = data.last_track["url"];
+  document.getElementById("cover").src = coverurl;
+  document.getElementById("bg-cover").src = coverurl;
+  document.getElementById("cover-link").href = data.last_track["url"];
+  document.getElementById("fmtime").innerHTML = "(" + data.date + ")";
+
+  document.getElementById("lastfm-scrobbles").innerText =
+    data.info["user"]["playcount"];
+
+  // RAVEPOP has an 18+ album cover on last.fm... plz fix :(
+  if (
+    last_track["album"]["#text"] == "RAVEPOP" &&
+    artist_name == "r u s s e l b u c k"
+  ) {
+    document.getElementById("cover").style.filter = "blur(8px)";
+  }
+
+  // if (artist_name == "Nirvana") {
+  //   const kurt = document.createElement("img");
+  //   kurt.src = "/assets/kurt.png";
+  //   kurt.loading = "lazy";
+  //   kurt.id = "kurt";
+  //   document.getElementById("lastfm").appendChild(kurt);
+  // }
+}
+
+main();
