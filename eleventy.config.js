d017eeacdc3f87d251012415be4b8786f80041e4
Sharky <sharky@furshark.net>
2025-08-08 19:06:08 +0200

    updated favicon.ico and added some copy passthrough stuff

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,6 +9,10 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/shark");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/elements.css");
+  eleventyConfig.addPassthroughCopy("src/cursor.png");
+
   eleventyConfig.addCollection("shark_posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/shark/**/*.{html,md}");
   });
