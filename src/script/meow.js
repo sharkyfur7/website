c4dc2e8baa53048623456a851515eb96eafc9a07
Sharky <sharky@furshark.net>
2025-10-28 14:34:50 +0100

    added a toggle for cat sounds

--- src/script/meow.js
+++ src/script/meow.js
@@ -1,3 +1,26 @@
+var checkbox = document.getElementById("catsounds");
+
+// https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -8,7 +31,23 @@ function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
+function toggleCatSounds() {
+  if (checkbox.checked) {
+    setCookie("catsound", true, 10);
+  } else {
+    setCookie("catsound", false, 10);
+  }
+}
+
+checkbox.checked = getCookie("catsound") || false;
+
+checkbox.addEventListener("change", toggleCatSounds, false);
+
 document.addEventListener("keydown", (event) => {
+  if (checkbox.checked) {
+    return;
+  }
+
   var i = getRandomInt(1, 29);
   i = i.toString().padStart(2, "0");
   var filename = i + ".wav"; // i am a horrible person
