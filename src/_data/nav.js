c65386caf2ebb2c787c7642d6f9566335299c106
Sharky <sharky@furshark.net>
2026-04-05 17:16:57 +0200

    I like space

---
+++ src/_data/nav.js
@@ -0,0 +1,13 @@
+export default function () {
+  return [
+    { label: "Home", url: "/" },
+    { label: "Links", url: "/links/" },
+    { label: "About", url: "/about/" },
+    { label: "Contact", url: "/contact/" },
+    { label: "Collections", url: "/collections/" },
+    { label: "!!spacer", url: "" },
+    { label: "Guestbook", url: "/guestbook/" },
+    { label: "Blog", url: "/blog/" },
+    { label: "Sharks", url: "/shark/" },
+  ];
+}
