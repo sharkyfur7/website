d8faf3d28da29c09a7891008e6281876b4eb8507
Sharky <sharky@furshark.net>
2026-04-06 17:20:35 +0200

    Idk lowkey

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -2,12 +2,9 @@ export default function () {
   return [
     { label: "Home", url: "/" },
     { label: "Links", url: "/links/" },
-    { label: "About", url: "/about/" },
-    { label: "Contact", url: "/contact/" },
     { label: "Collections", url: "/collections/" },
-    { label: "!!spacer", url: "" },
+    // { label: "!!spacer", url: "" },
     { label: "Guestbook", url: "/guestbook/" },
-    { label: "Blog", url: "/blog/" },
     { label: "Sharks", url: "/shark/" },
   ];
 }
